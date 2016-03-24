398cfaca5edd9bf439b150dc7f9b25af896b5a18
risacan <supikateriburu@gmail.com>
2016-03-24 23:29:32 +0900

    :sparkles: Try to get weather
    Thanks to http://qiita.com/nownabe/items/aeac1ce0977be963a740

---
+++ get_weather.rb
@@ -0,0 +1,13 @@
+require "dotenv"
+require "json"
+require "open-uri"
+require "pp"
+
+Dotenv.load
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
+API_KEY = ENV["API_KEY"]
+
+response = open(BASE_URL + "?zip=160-0004,JP&APPID=#{API_KEY}")
+weather_string = JSON.pretty_generate(JSON.parse(response.read))
+pp JSON.parse(weather_string, symbolize_names: true)

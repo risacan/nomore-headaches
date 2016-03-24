f35db312a6e42d1532160bd6f26dfb168cad38ae
risacan <supikateriburu@gmail.com>
2016-03-24 23:50:46 +0900

    :thinking_face: Try to see the pressure forecast

--- get_weather.rb
+++ get_weather.rb
@@ -10,4 +10,9 @@ API_KEY = ENV["API_KEY"]
 
 response = open(BASE_URL + "?zip=160-0004,JP&APPID=#{API_KEY}")
 weather_string = JSON.pretty_generate(JSON.parse(response.read))
-pp JSON.parse(weather_string, symbolize_names: true)
+forecast = JSON.parse(weather_string, symbolize_names: true)
+forecast[:list].each do |x|
+  puts Time.at(x[:dt])
+  puts x[:main][:pressure]
+  puts "---------------------"
+end
